e46d5be364d4f7d6ced7292271bfac0e8c081024
venka <venkateshkondeti199@gmail.com>
2025-06-29 22:38:46 -0500

    add New

--- src/Java_Lab_001/Program_01.java
+++ src/Java_Lab_001/Program_01.java
@@ -2,6 +2,6 @@ package Java_Lab_001;
 
 public class Program_01 {
     public static void main(String[] args) {
-        System.out.println("hello program");
+        System.out.println("hello program 1");
     }
 }
