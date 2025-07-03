5886276dd9dffa603b6ffda9d64379db4b045a7e
venka <venkateshkondeti199@gmail.com>
2025-07-03 17:55:25 -0500

    Ninth table update

--- src/Java_Lab_001/Ninth_table.java
+++ src/Java_Lab_001/Ninth_table.java
@@ -4,8 +4,7 @@ public class Ninth_table {
     public static void main(String[] args) {
 
         System.out.printf(
-                "9 * 1 = %d%n9 * 2 = %d%n9 * 3 = %d%n9 * 4 = %d%n9 * 5 = %d%n" +
-                        "9 * 6 = %d%n9 * 7 = %d%n9 * 8 = %d%n9 * 9 = %d%n9 * 10 = %d%n",
+                "9 * 1 = %d%n9 * 2 = %d%n9 * 3 = %d%n9 * 4 = %d%n9 * 5 = %d%n9 * 6 = %d%n9 * 7 = %d%n9 * 8 = %d%n9 * 9 = %d%n9 * 10 = %d%n",
                 9 * 1, 9 * 2, 9 * 3, 9 * 4, 9 * 5, 9 * 6, 9 * 7, 9 * 8, 9 * 9, 9 * 10);
     }
 }
