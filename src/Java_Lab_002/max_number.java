5ee98a66353301c3d937208f3b37989a30ac8544
venka <venkateshkondeti199@gmail.com>
2025-07-04 23:26:28 -0500

    Max_number

---
+++ src/Java_Lab_002/max_number.java
@@ -0,0 +1,10 @@
+package Java_Lab_002;
+
+public class max_number {
+    public static void main(String[] args) {
+        int a=10, b=20;
+        int max=(a>b)?a:b;
+        System.out.println(max);
+
+    }
+}
