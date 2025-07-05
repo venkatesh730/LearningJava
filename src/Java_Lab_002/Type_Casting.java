9e0f08976a614da8110eb1fadf7b9d2484b1b260
venka <venkateshkondeti199@gmail.com>
2025-07-05 00:28:28 -0500

    Type casting

---
+++ src/Java_Lab_002/Type_Casting.java
@@ -0,0 +1,11 @@
+package Java_Lab_002;
+
+public class Type_Casting {
+    public static void main(String[] args) {
+        int a=100;
+        double d=a;
+        System.out.println(d);
+
+
+    }
+}
