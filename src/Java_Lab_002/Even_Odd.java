f9c2f9a0bfe948a7c5e9c3a6308f78f853d16488
venka <venkateshkondeti199@gmail.com>
2025-07-04 23:42:38 -0500

    Even_odd

---
+++ src/Java_Lab_002/Even_Odd.java
@@ -0,0 +1,9 @@
+package Java_Lab_002;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        int n=20;
+        String result=(n%2==0)?"even":"odd";
+        System.out.println(result);
+    }
+}
